5f4a288c051a496fedfa4be441382d8111727cb8
Niklas <niklas@lindorfer.com>
2019-11-05 12:27:12 +0000

    Minor change to trigger CI

--- app/src/main/java/com/example/flirapp/MainActivity.java
+++ app/src/main/java/com/example/flirapp/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Test
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
