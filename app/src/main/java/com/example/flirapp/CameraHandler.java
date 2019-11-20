864d4a564f2969852c39a01cd1a670cba8a35786
Niklas <niklas@lindorfer.com>
2019-11-20 15:14:01 +0000

    Added camera discovery and connection

---
+++ app/src/main/java/com/example/flirapp/CameraHandler.java
@@ -0,0 +1,107 @@
+package com.example.flirapp;
+
+import androidx.annotation.Nullable;
+
+import com.flir.thermalsdk.live.Camera;
+import com.flir.thermalsdk.live.CommunicationInterface;
+import com.flir.thermalsdk.live.Identity;
+import com.flir.thermalsdk.live.connectivity.ConnectionStatusListener;
+import com.flir.thermalsdk.live.discovery.DiscoveryEventListener;
+import com.flir.thermalsdk.live.discovery.DiscoveryFactory;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+public class CameraHandler {
+
+    // Discovered FLIR cameras
+    private LinkedList<Identity> foundCameraIdentities = new LinkedList<>();
+
+    // Connected FLIR Camera
+    private Camera camera;
+
+    /**
+     * Possible discovery statuses
+     */
+    public interface DiscoveryStatus {
+        void started();
+        void stopped();
+    }
+
+    /**
+     * Empty constructor for now
+     */
+    public CameraHandler() { }
+
+    /**
+     * Start discovery of USB and Emulators
+     * @param cameraDiscoveryListener -
+     * @param discoveryStatus Current discovery status
+     */
+    public void startDiscovery(DiscoveryEventListener cameraDiscoveryListener, DiscoveryStatus discoveryStatus) {
+        DiscoveryFactory.getInstance().scan(cameraDiscoveryListener, CommunicationInterface.EMULATOR, CommunicationInterface.USB);
+        discoveryStatus.started();
+    }
+
+    /**
+     * Stop discovery of USB and Emulators
+     * @param discoveryStatus Current discovery status
+     */
+    public void stopDiscovery(DiscoveryStatus discoveryStatus) {
+        DiscoveryFactory.getInstance().stop(CommunicationInterface.EMULATOR, CommunicationInterface.USB);
+        discoveryStatus.stopped();
+    }
+
+    /**
+     * Connect to a FLIR One camera
+     * @param identity Identity of a discovered FLIR camera
+     * @param connectionStatusListener Event listener
+     */
+    public void connect(Identity identity, ConnectionStatusListener connectionStatusListener) {
+        camera = new Camera();
+        camera.connect(identity, connectionStatusListener);
+    }
+
+    /**
+     * Disconnect a FLIR One camera
+     */
+    public void disconnect() {
+        if (camera == null) {
+            return;
+        }
+        if (camera.isGrabbing()) {
+            camera.unsubscribeAllStreams();
+        }
+        camera.disconnect();
+    }
+
+    /**
+     * Add a found camera to the list of known cameras
+     */
+    public void add(Identity identity) {
+        foundCameraIdentities.add(identity);
+    }
+
+    @Nullable
+    public Identity get(int i) {
+        return foundCameraIdentities.get(i);
+    }
+
+    /**
+     * Get a read only list of all found cameras
+     */
+    @Nullable
+    public List<Identity> getCameraList() {
+        return Collections.unmodifiableList(foundCameraIdentities);
+    }
+
+    /**
+     * Clear all known network cameras
+     */
+    public void clear() {
+        foundCameraIdentities.clear();
+    }
+
+
+}
--- app/src/main/java/com/example/flirapp/MainActivity.java
+++ app/src/main/java/com/example/flirapp/MainActivity.java
@@ -3,17 +3,31 @@ package com.example.flirapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
+import com.flir.thermalsdk.ErrorCode;
 import com.flir.thermalsdk.androidsdk.ThermalSdkAndroid;
 import com.flir.thermalsdk.androidsdk.live.connectivity.UsbPermissionHandler;
+import com.flir.thermalsdk.live.CommunicationInterface;
+import com.flir.thermalsdk.live.Identity;
+import com.flir.thermalsdk.live.discovery.DiscoveryEventListener;
 import com.flir.thermalsdk.log.ThermalLog;
-//import com.flir.thermalsdk.Disc
+
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
+
+    private CameraHandler cameraHandler;
+    private Identity connectedIdentity = null;
     private UsbPermissionHandler usbPermissionHandler = new UsbPermissionHandler();
 
 
+    /**
+     * Executed when activity is created
+     * @param savedInstanceState -
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +38,109 @@ public class MainActivity extends AppCompatActivity {
 
         // Initialize SDK
         ThermalSdkAndroid.init(getApplicationContext(), enableLoggingInDebug);
-//        DiscoveryManager.getInstance().scan(aDiscoveryEventListener, CommunicationInterface.USB);
+
+        cameraHandler = new CameraHandler();
+
+        startDiscovery();
     }
+
+
+    /**
+     * Start camera discovery
+     */
+    private void startDiscovery() {
+        cameraHandler.startDiscovery(cameraDiscoveryListener, discoveryStatusListener);
+    }
+
+    /**
+     * Stop camera discovery
+     */
+    private void stopDiscovery() {
+        cameraHandler.stopDiscovery(discoveryStatusListener);
+    }
+
+    /**
+     * Callback for discovery status
+     */
+    private CameraHandler.DiscoveryStatus discoveryStatusListener = new CameraHandler.DiscoveryStatus() {
+        @Override
+        public void started() {
+            showMessage.show("Starting discovery.");
+        }
+
+        @Override
+        public void stopped() {
+            showMessage.show("Stopping discovery.");
+        }
+    };
+
+    /**
+     * Callback for camera discovered
+     */
+    private DiscoveryEventListener cameraDiscoveryListener = new DiscoveryEventListener() {
+
+        @Override
+        public void onCameraFound(Identity identity) {
+            Log.d(TAG, "onCameraFound identity:" + identity);
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    showMessage.show("Device discovered!");
+                    cameraHandler.add(identity);
+                }
+            });
+        }
+
+        @Override
+        public void onDiscoveryError(CommunicationInterface communicationInterface, ErrorCode errorCode) {
+            Log.d(TAG, "onDiscoveryError communicationInterface:" + communicationInterface + " errorCode:" + errorCode);
+
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    stopDiscovery();
+                    showMessage.show("onDiscoveryError communicationInterface:" + communicationInterface + " errorCode:" + errorCode);
+                }
+            });
+        }
+    };
+
+    /**
+     * Connect to a Camera
+     */
+    private void connect(Identity identity) {
+
+        // Stop discovery when connected
+        cameraHandler.stopDiscovery(discoveryStatusListener);
+
+        // Already connected
+        if (connectedIdentity != null) {
+            Log.d(TAG, "connect(), already connected to a camera!");
+            showMessage.show("connect(), already connected to a camera!");
+            return;
+        }
+
+        // No camera available
+        if (identity == null) {
+            Log.d(TAG, "connect(), no camera available!");
+            showMessage.show("connect(), no camera available!");
+            return;
+        }
+
+        connectedIdentity = identity;
+    }
+
+    /**
+     * Show message on the screen
+     */
+    public interface ShowMessage {
+        void show(String message);
+    }
+
+    private ShowMessage showMessage = new ShowMessage() {
+        @Override
+        public void show(String message) {
+            Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
+        }
+    };
 }
