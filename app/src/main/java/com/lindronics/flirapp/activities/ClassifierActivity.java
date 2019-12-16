d5c6069931435fbf7efa84ada3dd834e01b262d7
Niklas <niklas@lindorfer.com>
2019-12-16 12:41:36 +0000

    ClassifierActivity now extends AbstractCameraActivity

--- app/src/main/java/com/lindronics/flirapp/activities/ClassifierActivity.java
+++ app/src/main/java/com/lindronics/flirapp/activities/ClassifierActivity.java
@@ -5,41 +5,24 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.HandlerThread;
 import android.util.Log;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.UiThread;
-import androidx.appcompat.app.AppCompatActivity;
 
-import com.flir.thermalsdk.ErrorCode;
-import com.flir.thermalsdk.live.Identity;
-import com.flir.thermalsdk.live.connectivity.ConnectionStatus;
-import com.flir.thermalsdk.live.connectivity.ConnectionStatusListener;
-import com.google.gson.Gson;
-import com.lindronics.flirapp.camera.CameraHandler;
 import com.lindronics.flirapp.R;
 import com.lindronics.flirapp.camera.FrameDataHolder;
 import com.lindronics.flirapp.classification.ModelHandler;
 
-import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
 import java.util.List;
-import java.util.concurrent.LinkedBlockingQueue;
 
-public class ClassifierActivity extends AppCompatActivity {
+public class ClassifierActivity extends AbstractCameraActivity {
 
     private static final String TAG = "ClassifierActivity";
 
     private Handler handler;
     private HandlerThread handlerThread;
-
-    private CameraHandler cameraHandler;
-
-    private ImageView rgbImage;
-    private ImageView firImage;
-
-    private LinkedBlockingQueue<FrameDataHolder> framesBuffer = new LinkedBlockingQueue<>(21);
 
     private ModelHandler modelHandler;
 
@@ -50,31 +33,12 @@ public class ClassifierActivity extends AppCompatActivity {
     /**
      * Executed when activity is created.
      * Get camera identity from intent and connect to camera.
-     *
      */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_classifier);
+        super.onCreate(savedInstanceState);
 
-        cameraHandler = new CameraHandler();
-
-        rgbImage = findViewById(R.id.rgb_view);
-        firImage = findViewById(R.id.fir_view);
-
-        Bundle extras = getIntent().getExtras();
-        Gson gson = new Gson();
-
-        if (extras == null) {
-            finish();
-            return;
-        }
-
-        String identityString = extras.getString("cameraIdentity");
-        Identity cameraIdentity = gson.fromJson(identityString, Identity.class);
-        cameraHandler.connect(cameraIdentity, connectionStatusListener);
-
-//        modelHandler = new ModelHandler(this, ModelHandler.Device.GPU, 1);
         recreateModelHandler();
 
         firstPredictionBox = findViewById(R.id.first_prediction_box);
@@ -103,59 +67,41 @@ public class ClassifierActivity extends AppCompatActivity {
         super.onPause();
     }
 
+    /**
+     * Run procedure in background
+     * @param r Runnable to run
+     */
     private synchronized void runInBackground(final Runnable r) {
         if (handler != null) {
             handler.post(r);
         }
     }
 
-    /**
-     * Defines behaviour for when images are received
-     */
-    private final CameraHandler.StreamDataListener streamDataListener = new CameraHandler.StreamDataListener() {
+    @Override
+    public void images(FrameDataHolder dataHolder) {
+        super.images(dataHolder);
+    }
 
-        @Override
-        public void images(FrameDataHolder dataHolder) {
+    @Override
+    public void images(Bitmap firBitmap, Bitmap rgbBitmap) {
+        super.images(firBitmap, rgbBitmap);
 
-            runOnUiThread(() -> {
-                firImage.setImageBitmap(dataHolder.firBitmap);
-                rgbImage.setImageBitmap(dataHolder.rgbBitmap);
-            });
-        }
+        // Run classification
+        runInBackground(() -> {
+            if (modelHandler != null) {
+                final List<ModelHandler.Recognition> results =
+                        modelHandler.recognizeImage(rgbBitmap, firBitmap);
+                Log.i("UPDATE", "Confidence: " + results.get(0).getConfidence());
 
-        @Override
-        public void images(Bitmap firBitmap, Bitmap rgbBitmap) {
-
-            try {
-                framesBuffer.put(new FrameDataHolder(firBitmap, rgbBitmap));
-            } catch (InterruptedException e) {
-                // If interrupted while waiting for adding a new item in the queue
-                Log.e(TAG, "images(), unable to add incoming images to frames buffer, exception:" + e);
+                runOnUiThread(() -> showResultsInBottomSheet(results));
             }
+        });
+    }
 
-            runOnUiThread(() -> {
-                Log.d(TAG, "framebuffer size:" + framesBuffer.size());
-                FrameDataHolder poll = framesBuffer.poll();
-                if (poll != null) {
-                    firImage.setImageBitmap(poll.firBitmap);
-                    rgbImage.setImageBitmap(poll.rgbBitmap);
-                }
-            });
-
-            // Run classification
-            runInBackground(() -> {
-                if (modelHandler != null) {
-                    final List<ModelHandler.Recognition> results =
-                            modelHandler.recognizeImage(rgbBitmap, firBitmap);
-                    Log.i("UPDATE", "Confidence: " + results.get(0).getConfidence());
-
-                    runOnUiThread(() -> showResultsInBottomSheet(results));
-                }
-            });
-
-        }
-    };
-
+    /**
+     * Display classification results
+     * @param results List of results
+     */
     @UiThread
     private void showResultsInBottomSheet(List<ModelHandler.Recognition> results) {
         if (results != null && results.size() >= 2) {
@@ -171,6 +117,10 @@ public class ClassifierActivity extends AppCompatActivity {
         }
     }
 
+    /**
+     * Reload model handler.
+     * Finish activity if unsuccessful.
+     */
     private void recreateModelHandler() {
         if (modelHandler != null) {
             modelHandler.close();
@@ -187,30 +137,8 @@ public class ClassifierActivity extends AppCompatActivity {
 
 
     /**
-     * Defines behaviour vor changes of the connection status
+     * Behaviour already covered by superclass
      */
-    private final ConnectionStatusListener connectionStatusListener = new ConnectionStatusListener() {
-        @Override
-        public void onConnectionStatusChanged(@NotNull ConnectionStatus connectionStatus, @org.jetbrains.annotations.Nullable ErrorCode errorCode) {
-            Log.d(TAG, "onConnectionStatusChanged connectionStatus:" + connectionStatus + " errorCode:" + errorCode);
-
-            runOnUiThread(() -> {
-
-                switch (connectionStatus) {
-                    case CONNECTING:
-                        break;
-                    case CONNECTED: {
-                        cameraHandler.startStream(streamDataListener);
-                    }
-                    break;
-                    case DISCONNECTING:
-                        break;
-                    case DISCONNECTED: {
-                        finish();
-                    }
-                    break;
-                }
-            });
-        }
-    };
+    @Override
+    void onDisconnected() { }
 }
